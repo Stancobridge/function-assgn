738f27b68b4ebb8c03911973c1ae7e694b0b1737
John Ifemezuo .A <49494778+smallville1998@users.noreply.github.com>
2019-07-15 00:35:48 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,32 +1,105 @@
+// WRITE A SIMPLE PROGRAM 
+// THE OUTPUT SHOULD LOOK LIKE THIS
 
-const member = {};
-var displayOutPut;
-var addMember;
+// ====================
+/* STUDENT INFORMATION 
+   ====================
 
- var createNewLine = function () {
-   return "\n";
+   NAME: JOHN IFEMEZUO ANAYO
+   REG.NO: FPOCSNB17027
+   LEVEL:ND1 REGULAR II */
+
+// THE INFORMATION YOU INPUT CARRIES ARRAYS OF NAME.
+
+var CreatDetails;
+var arrayOfStudent;
+var displaydetail;
+
+var spacers = {
+    newline : function () {
+         return "\n";
+    },
+
+    border : function () {
+        var simbol;
+        simbol = "====================="
+        return simbol;
+    },
+    
 }
-var newLine = createNewLine() ;
 
-displayOutPut = function (name, sex) {
-         member.name = name;
-         member.sex  = sex;
+var newline = spacers.newline();
+var border  = spacers.border();
 
-         member.displayMember = function () {
-             var info;
-             info =  "Name" + " : "   + this.name ;
-             info += " " + newLine;
-             info += "Gender/sex : " + this.sex;
-             console.log(info);
-         }
+var label = function (hold) {
+    hold = border + newline;
+    hold += "Registerd Information" + newline;
+    hold += border;
+    return hold
+}
 
-         return member;
+CreatDetails = function (name,reg,level,program) {
+    this.name =  name;
+    this.reg  =  reg;
+    this.level = level;
+    this.program = program;
+
+    this.showdetails = function () {
+        var register;
+        register = "Name    : " + this.name + newline;
+        register += "Reg.No  : " + this.reg + newline;
+        register += "Level   : " + this.level + newline;
+        register += "Program : " + this.program ;
+        return register
     }
+}
 
-addMember = [displayOutPut("Okechukwu Somtokwo","Male"),
-             displayOutPut("john ifem","Male"),
-             displayOutPut("Thompson IdontKnow","Male")];
 
-addMember.forEach(function (name) {
-    name.displayMember()
-})s
+arrayOfStudent = {
+    nd1 : [
+        {
+            name : "victor Tochukwu",
+            "Reg.No" : "FPOCSNB17027",
+            level : "Nd2",
+            program: "regular 1"
+        },
+        {
+            name : "Sam Chimaoge",
+            "Reg.No" : "FPOCSNB17030",
+            level : "Nd2",
+            program: "regular 2"
+        },
+        {
+            name : "Onyebuchi Mmeso",
+            "Reg.No" : "FPOCSNB17001",
+            level : "Nd2",
+            program: "regular 1"
+        },
+        {
+            name : "Stanley Tochukwu",
+            "Reg.No" : "FPOCSNB17027",
+            level : "Hnd3",
+            program: "On IT right know."
+        },
+        {
+            name : "Godwin Idontknowhisothername",
+            "Reg.No" : "FPOCSNB17000",
+            level : "Hnd4",
+            program: "On IT right know."
+        },
+    ]
+}
+
+console.log(label())
+detailfunc = new CreatDetails(arrayOfStudent.nd1[0].name,arrayOfStudent.nd1[0]["Reg.No"],arrayOfStudent.nd1[0].level,arrayOfStudent.nd1[0].program);
+console.log(detailfunc.showdetails())
+
+console.log(border);
+detailfunc = new CreatDetails(arrayOfStudent.nd1[3].name,arrayOfStudent.nd1[3]["Reg.No"],arrayOfStudent.nd1[3].level,arrayOfStudent.nd1[3].program);
+console.log(detailfunc.showdetails())
+
+console.log(border);
+detailfunc = new CreatDetails(arrayOfStudent.nd1[4].name,arrayOfStudent.nd1[4]["Reg.No"],arrayOfStudent.nd1[4].level,arrayOfStudent.nd1[4].program);
+console.log(detailfunc.showdetails())
+
+
